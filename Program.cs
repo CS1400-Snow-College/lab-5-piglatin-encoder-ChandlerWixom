fc2d2a142096a0be9b369b4a3178f19bb88ee84c
Chandler Wixom <chandlerj.wixom@gmail.com>
2025-02-26 14:25:15 -0700

    collet a string

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// Chandler Wixom, 2/26/2025, Lab 5 Piglatin / Encoder 
 Console.WriteLine("This program will convert anything you type into Piglatin and encrypt it aswell");
-
+Console.WriteLine("Please enter your message: ");
+string userMessage = Console.ReadLine();
